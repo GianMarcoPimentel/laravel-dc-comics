46cf0d800d527999c280037ec2a1f5e58706cb0b
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-04-11 16:05:34 +0200

    Cambio la Route

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get("/", [MovieController::class, 'index'] )->name('home');
+/* 
+Route::get("/", [MovieController::class, 'index'] )->name('home'); */
+Route::resource('movies', MovieController::class);
