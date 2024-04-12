b79a46e29da0d23ad14503e3ff68cf3acaa1e807
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-04-12 16:27:27 +0200

    feat: modal for delete comic

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import './bootstrap';
 import '~resources/scss/app.scss'
 
 /* import * as bootstrap from 'bootstrap'; */
+import '~bootstrap/js/index.esm.js'
 
 
 
--- resources/views/comic/show.blade.php
+++ resources/views/comic/show.blade.php
@@ -25,15 +25,42 @@
 </div>
 <div class="container py-5">
     <a href="{{ route('movies.edit', $movie->id) }}" class="btn btn-primary">Modifica</a>
+
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Elimina
+  </button>
+  
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Sei sicuro di voler eliminare questo comic?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Torna </button>
+          
+          <form action="{{ route('movies.destroy', $movie->id) }}" method="POST">
+            @csrf
+            @method("DELETE")
+           {{--  <button type="button" class="btn btn-danger">Elimina<button> --}}
+            <input type="submit" class="btn btn-danger" value="Elimina">
+         </form>
+        </div>
+      </div>
+    </div>
+  </div>
    
     
     
-    <form action="{{ route('movies.destroy', $movie->id) }}" method="POST">
-        @csrf
-        @method("DELETE")
-    <button class="btn btn-danger">Elimina<button>
     
-    </form>
+
+    
 </div>
 @endsection
 
