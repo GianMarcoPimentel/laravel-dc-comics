dc756e216da206f1341d8452fe5d6e14be64ce2a
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-04-15 15:56:45 +0200

    uso operatore ternario di laravel

--- resources/views/comic/edit.blade.php
+++ resources/views/comic/edit.blade.php
@@ -15,47 +15,47 @@
 
         <div class="mb-3">
           <label for="title" class="form-label">Nome del film</label>
-          <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{$movie->title}}">
+          <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title') ? old('title') : $movie->title}}">
         </div>
         
         <div class="mb-3">
           <label for="description" class="form-label">Descrizione</label>
-          <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description" name=" description">value="{{$movie->description}}"</textarea >
+          <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description" name=" description">value="{{old('description') ?? $movie->description}}"</textarea >
         </div>
 
         <div class="mb-3 ">
           <label class="form-label" for="thumb">Immagine</label>
-          <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name=" thumb" value="{{$movie->thumb}}">
+          <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name=" thumb" value="{{old('thumb') ?? $movie->thumb}}">
         </div>
 
         <div class="mb-3 ">
           <label class="form-label" for="price">Prezzo</label>
-          <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name=" price" value="{{$movie->price}}">
+          <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name=" price" value="{{old('price') ?? $movie->price}}">
         </div>
           
         <div class="mb-3 ">
           <label class="form-label" for="series">Serie</label>
-          <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name=" series" value="{{$movie->series}}">
+          <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name=" series" value="{{old('series') ?? $movie->series}}">
         </div>
   
         <div class="mb-3 ">
           <label class="form-label" for="sale_date">Uscita</label>
-          <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name=" sale_date" value="{{$movie->sale_date}}">
+          <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name=" sale_date" value="{{old('sale_date') ?? $movie->sale_date}}">
         </div>
 
         <div class="mb-3 ">
           <label class="form-label" for="type">Tipologia</label>
-          <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name=" type" value="{{$movie->type}}">
+          <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name=" type" value="{{old('type') ?? $movie->type}}">
         </div>
 
         <div class="mb-3 ">
           <label class="form-label" for="artists">Artisti</label>
-          <input artists="text" class="form-control @error('artists') is-invalid @enderror" id="type" name=" artists" value="{{$movie->artists}}">
+          <input artists="text" class="form-control @error('artists') is-invalid @enderror" id="type" name=" artists" value="{{old('artists') ?? $movie->artists}}">
         </div>
 
         <div class="mb-3 ">
           <label class="form-check-label" for="writers">Scrittori</label>
-          <input type="text" class="form-control @error('writers') is-invalid @enderror" id="writers" name=" writers" value="{{$movie->writers}}">
+          <input type="text" class="form-control @error('writers') is-invalid @enderror" id="writers" name=" writers" value="{{old('writers') ?? $movie->writers}}">
         </div>
 
         {{-- mostro l'eventuale errore in pagina --}}
